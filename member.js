d50508f086ab9038b2e7476d1b12d4bd03306cf3
ekolink <47786085+ekolink@users.noreply.github.com>
2025-02-14 14:22:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,4 @@
+function skillsMember() {
+    var skills = ['Nodejs', 'React', 'Angular', 'Vue', 'MongoDB', 'MySQL', 
+        'PostgreSQL', 'Python', 'Django', 'Flask', 'Javascript', 'Typescript', 'HTML', 'CSS', 'SASS', 'LESS',]
+    }
